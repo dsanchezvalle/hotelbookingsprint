53c333e8c2ce778cbd4f7f02144cbd599a7c0fc1
David Sanchez <dsanva2004@gmail.com>
2021-05-21 10:29:27 -0500

    feat:adjust content of HotelCard component for mobile version

--- src/components/Main/HotelCard/HotelCard.jsx
+++ src/components/Main/HotelCard/HotelCard.jsx
@@ -3,20 +3,19 @@ import React from 'react';
 const HotelCard = () => {
     return (
         <article className="HotelCard">
-            <h2 className="HotelCard__Title">Hotel **** Name</h2>
+            <h2 className="HotelCard__Title">La Bamba de Areco</h2>
             <section className="HotelCard__Info">
-                
-                <p className="HotelCard__Country">
-                    <img src="" alt="(flag)" className="HotelCard__Flag" />
+                <div className="HotelCard__Country">
+                    <img src="https://cdn.countryflags.com/thumbs/argentina/flag-round-250.png" alt="(flag)" className="HotelCard__Flag" />
                     <p className="HotelCard__CountryName">Argentina</p> 
-                </p>
+                </div>
                 <div className="HotelCard__Numbers">
-                    <p className="HotelCard__Rooms">## Rooms</p>
+                    <p className="HotelCard__Rooms">22 Rooms -</p>
                     <p className="HotelCard__Price">$$$$</p>
                 </div>
                 <p className="HotelCard__Description">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis assumenda suscipit, consequuntur possimus mollitia minima commodi eius eos, temporibus porro sequi id ad molestiae dolores ea perferendis. Adipisci, voluptas magni?</p>
-                <button className="HotelCard__Button">Book it!</button>
             </section>
+            <button className="HotelCard__Button">Book it!</button>
         </article>
     );
 }
