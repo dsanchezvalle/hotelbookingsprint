845bc47d025bba1052e03b65c130dba39e07b5a6
David Sanchez <dsanva2004@gmail.com>
2021-05-21 17:01:29 -0500

    feat:add hotels data, send props to HotelCard from HotelList component and render one test HotelCard

--- src/components/Main/HotelList/HotelList.jsx
+++ src/components/Main/HotelList/HotelList.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import HotelCard from '../HotelCard/HotelCard'
+import { hotelsData } from '../../../assets/data/data.js'
 
 const HotelList = () => {
     return(
         <section className="HotelsContainer">
-            <HotelCard />
-            <HotelCard />
-            <HotelCard />
-            <HotelCard />
-            <HotelCard />
-            <HotelCard />
-            <HotelCard />
-            <HotelCard />
-            <HotelCard />
-            <HotelCard />
-            <HotelCard />
-            <HotelCard />
-            <HotelCard />
-            <HotelCard />
+            <HotelCard 
+                slug = {hotelsData[0].slug}
+                name = {hotelsData[0].name}
+                photo = {hotelsData[0].photo}
+                description = {hotelsData[0].description}
+                availabilityFrom = {hotelsData[0].availabilityFrom}
+                availabilityTo = {hotelsData[0].availabilityTo}
+                rooms = {hotelsData[0].rooms}
+                city = {hotelsData[0].city}
+                country = {hotelsData[0].country}
+                price = {hotelsData[0].price}
+            />
         </section>
     );
 }
