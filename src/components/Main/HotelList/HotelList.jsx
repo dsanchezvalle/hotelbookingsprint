a9eab21d2786c7cc5f53b6a5ff5457f85ac93072
David Sanchez <dsanva2004@gmail.com>
2021-05-21 16:37:02 -0500

    feat: create HotelList component and insert it in the Main component

---
+++ src/components/Main/HotelList/HotelList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import HotelCard from '../HotelCard/HotelCard'
+
+const HotelList = () => {
+    return(
+        <section className="HotelsContainer">
+            <HotelCard />
+            <HotelCard />
+            <HotelCard />
+            <HotelCard />
+            <HotelCard />
+            <HotelCard />
+            <HotelCard />
+            <HotelCard />
+            <HotelCard />
+            <HotelCard />
+            <HotelCard />
+            <HotelCard />
+            <HotelCard />
+            <HotelCard />
+        </section>
+    );
+}
+
+export default HotelList;
