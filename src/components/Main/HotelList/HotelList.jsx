7f3710b36ea0b650ea7cccd44ed6dff64d58bac9
David Sanchez <dsanva2004@gmail.com>
2021-05-21 17:14:05 -0500

    feat:add map to hotelsData list to iterate HotelCards

--- src/components/Main/HotelList/HotelList.jsx
+++ src/components/Main/HotelList/HotelList.jsx
@@ -5,20 +5,27 @@ import { hotelsData } from '../../../assets/data/data.js'
 const HotelList = () => {
     return(
         <section className="HotelsContainer">
-            <HotelCard 
-                slug = {hotelsData[0].slug}
-                name = {hotelsData[0].name}
-                photo = {hotelsData[0].photo}
-                description = {hotelsData[0].description}
-                availabilityFrom = {hotelsData[0].availabilityFrom}
-                availabilityTo = {hotelsData[0].availabilityTo}
-                rooms = {hotelsData[0].rooms}
-                city = {hotelsData[0].city}
-                country = {hotelsData[0].country}
-                price = {hotelsData[0].price}
-            />
+            { hotelsData.map((hotel) => {
+                return(
+                    <HotelCard
+                    key = {"h-" + hotel.name} 
+                    slug = {hotel.slug}
+                    name = {hotel.name}
+                    photo = {hotel.photo}
+                    description = {hotel.description}
+                    availabilityFrom = {hotel.availabilityFrom}
+                    availabilityTo = {hotel.availabilityTo}
+                    rooms = {hotel.rooms}
+                    city = {hotel.city}
+                    country = {hotel.country}
+                    price = {hotel.price}
+                />
+                );
+            })    
+            }
         </section>
     );
 }
 
 export default HotelList;
+
