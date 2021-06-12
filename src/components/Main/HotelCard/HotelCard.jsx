bdb512824f8d7e476fb156d94ff9e4ce20f5972a
David Sanchez <dsanva2004@gmail.com>
2021-06-12 17:21:19 -0500

    feat:add hotelcard:hover to present hotels description

--- src/components/Main/HotelCard/HotelCard.jsx
+++ src/components/Main/HotelCard/HotelCard.jsx
@@ -8,14 +8,16 @@ const HotelCard = ({name, country, rooms, price, description, photo}) => {
             <img className="HotelCard__Img" src={require(`../../../assets/images/Main/Hotels/${photo}`)} alt="photoHotel" />
             <h2 className="HotelCard__Title">{name}</h2>
             <section className="HotelCard__Info">
-                <div className="HotelCard__Country" >
-                    <img src={require(`../../../assets/images/Main/Flags/${country.toLowerCase()}.svg`)} alt="(flag)" className="HotelCard__Flag" />
-                    <p className="HotelCard__CountryName">{country}</p> 
-                </div>
-                <div className="HotelCard__Numbers">
-                    <p className="HotelCard__Rooms">{rooms} rooms</p>
-                    -
-                    <p className="HotelCard__Price">{getPriceSigns(price)}</p>
+                <div className="HotelCard__Country-Numbers">
+                    <div className="HotelCard__Country" >
+                        <img src={require(`../../../assets/images/Main/Flags/${country.toLowerCase()}.svg`)} alt="(flag)" className="HotelCard__Flag" />
+                        <p className="HotelCard__CountryName">{country}</p> 
+                    </div>
+                    <div className="HotelCard__Numbers">
+                        <p className="HotelCard__Rooms">{rooms} rooms</p>
+                        -
+                        <p className="HotelCard__Price">{getPriceSigns(price)}</p>
+                    </div>
                 </div>
                 <p className="HotelCard__Description">{description}</p>
             </section>
